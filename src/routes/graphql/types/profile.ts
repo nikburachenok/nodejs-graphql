369b8aa518e3803aaec8f1ae1b63a47aed596b51
Nikolay Burachenok <nikbur19@gmail.com>
2024-03-03 07:27:30 +0100

    feat: added profileType

---
+++ src/routes/graphql/types/profile.ts
@@ -0,0 +1,18 @@
+import {
+    GraphQLObjectType,
+    GraphQLNonNull,
+    GraphQLBoolean,
+    GraphQLInt
+} from "graphql";
+import { UUIDType } from "./uuid.js";
+
+export const ProfileType = new GraphQLObjectType({
+    name: "Profile",
+    fields: {
+        id: { type: new GraphQLNonNull(UUIDType) },
+        isMale: { type: new GraphQLNonNull(GraphQLBoolean) },
+        yearOfBirth: { type: new GraphQLNonNull(GraphQLInt) },
+        userId: { type: new GraphQLNonNull(UUIDType) },
+        memberTypeId: { type: new GraphQLNonNull(UUIDType) },
+    },
+})
