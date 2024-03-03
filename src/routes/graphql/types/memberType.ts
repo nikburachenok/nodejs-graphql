fe09e7a5435bf60ead1e874fca7479180e00bae8
Nikolay Burachenok <nikbur19@gmail.com>
2024-03-03 07:08:28 +0100

    feat: added member and memberId types

---
+++ src/routes/graphql/types/memberType.ts
@@ -0,0 +1,16 @@
+import {
+    GraphQLObjectType,
+    GraphQLNonNull,
+    GraphQLFloat,
+    GraphQLInt
+} from "graphql";
+import { MemberTypeIdGql } from "./memberTypeId.js";
+
+export const MemberType = new GraphQLObjectType({
+    name: "MemberType",
+    fields: {
+        id: { type: new GraphQLNonNull(MemberTypeIdGql) },
+        discount: { type: new GraphQLNonNull(GraphQLFloat) },
+        postsLimitPerMonth: { type: new GraphQLNonNull(GraphQLInt) }
+    },
+})
